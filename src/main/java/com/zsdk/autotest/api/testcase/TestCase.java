a0bddfcd086f31a55c7cdf6aa966897f79ee96fd
bond <80436007@qq.com>
2018-11-10 18:17:27 +0800

    增加用例步骤

--- src/main/java/com/zsdk/autotest/api/testcase/TestCase.java
+++ src/main/java/com/zsdk/autotest/api/testcase/TestCase.java
@@ -8,7 +8,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.apache.commons.lang.ObjectUtils.Null;
 import org.apache.http.client.ClientProtocolException;
 import org.testng.Reporter;
 
@@ -16,7 +15,6 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import org.testng.asserts.Assertion;
 
 import com.zsdk.autotest.common.Common;
 import com.zsdk.autotest.common.ExcelUtils;
@@ -26,7 +24,7 @@ public class TestCase {
 
 	public Map<String, HashMap> allTestCases;
 	// 测试案例文件路径--excel作为存储
-//	public String testCasePath = "E:/workspace/auto_test/testcases.xls";
+	// public String testCasePath = "E:/workspace/auto_test/testcases.xls";
 	public String testCasePath = "testcases.xls";
 
 	@BeforeClass
@@ -40,6 +38,46 @@ public class TestCase {
 
 	@AfterClass
 	public void afterClass() {
+	}
+
+	@Test(dataProvider = "case_a")
+	public void testApi(Map<String, Object> map) throws ClientProtocolException, IOException, URISyntaxException {
+		// CaseName RequestMethod RequestUrl RequestHead RequestData
+		// ExpectResult CaseDesc
+		String caseName = (String) map.get("CaseName");
+		Reporter.log("【用例名称】：" + map.get("CaseName") + "===========================================");
+
+		// 用例步骤
+		Map<String, String>[] maps = (Map<String, String>[]) map.get("Steps");
+		for (int i = 0; i < maps.length; i++) {
+			// Step RequestMethod RequestUrl RequestHead RequestData
+			// ResultComparisonType ExpectResult CaseDesc
+			String step = maps[i].get("Step");
+			Map<String, String> stepMap = new HashMap<>();
+			Reporter.log("用例:" + caseName + "  Step:" + step + " 开始执行......");
+			Reporter.log("【用例参数】：" + stepMap);
+			
+			Map<String, String> respMap = null;
+			if (i == 0) {
+				respMap = doHttpRequest(stepMap);
+			}else {
+				respMap = doHttpRequest(stepMap);
+			}
+			
+			Reporter.log("用例:" + caseName + "  Step:" + step + " 执行完成");
+			
+			Reporter.log("【响应结果】：" + map.get("CaseName") + "===========================================");
+			Reporter.log("respMap" + respMap);
+
+//			Reporter.log("【结果校验】：校验类型ResultComparisonType:" + resultComparisonType);
+//			Reporter.log("【结果校验】：expect:" + expect);
+
+//			compareResult(resultComparisonType, expect, respStr);
+
+		}
+
+		Reporter.log("【用例】：" + map.get("CaseName") + "执行完成！！！");
+
 	}
 
 	@DataProvider(name = "case_a")
@@ -105,14 +143,17 @@ public class TestCase {
 		compareResult(resultComparisonType, expect, respStr);
 	}
 
-	
 	/**
 	 * 响应结果与预期对比
-	 * @param resultComparisonType  对比类型 ：contain,equal,>,<
-	 * @param expect 期望值
-	 * @param respStr 响应结果
+	 * 
+	 * @param resultComparisonType
+	 *            对比类型 ：contain,equal,>,<
+	 * @param expect
+	 *            期望值
+	 * @param respStr
+	 *            响应结果
 	 */
-	public void compareResult(String resultComparisonType, String expect, String respStr) {
+	private void compareResult(String resultComparisonType, String expect, String respStr) {
 		switch (resultComparisonType) {
 		case "contain":
 			if (respStr.indexOf(expect) < 0) {
@@ -128,7 +169,7 @@ public class TestCase {
 			if (expect.equals(respStr)) {
 				assertTrue(true);
 				Reporter.log("【结果校验】：pass");
-			}else {
+			} else {
 				assertTrue(false);
 				Reporter.log("【结果校验】：fail");
 			}
@@ -138,7 +179,7 @@ public class TestCase {
 			if (respStr.compareTo(expect) > 0) {
 				assertTrue(true);
 				Reporter.log("【结果校验】：pass");
-			}else {
+			} else {
 				assertTrue(true);
 				Reporter.log("【结果校验】：pass");
 			}
@@ -148,7 +189,7 @@ public class TestCase {
 			if (respStr.compareTo(expect) < 0) {
 				assertTrue(true);
 				Reporter.log("【结果校验】：pass");
-			}else {
+			} else {
 				assertTrue(true);
 				Reporter.log("【结果校验】：pass");
 			}
@@ -159,10 +200,54 @@ public class TestCase {
 		}
 	}
 
-	// @Test
-	public void f3() {
+	private static Map<String, String> doHttpRequest(Map<String, String> map)
+			throws ClientProtocolException, IOException, URISyntaxException {
+
+		String url = map.get("RequestUrl");
+		url = Common.checkAndChangeUrl(url);
+		String method = map.get("RequestMethod");
+		String header = map.get("RequestHead");
+		String requestData = map.get("RequestData");
+
+		String resultComparisonType = map.get("ResultComparisonType");
+		String expect = map.get("ExpectResult");
+
+		Map<String, String> headMap = new HashMap<>();
+		String respStr = "";
+		if ("POST".equals(method)) {
+			if (header == null || header.isEmpty()) {
+				respStr = HttpClientUtil.doPostRequst(url, requestData);
+			} else {
+				headMap = Common.stringToMap(header, ":", '|');
+				respStr = HttpClientUtil.doPostRequst(url, requestData, headMap);
+			}
+		} else if ("GET".equals(method)) {
+			if (header == null || header.isEmpty()) {
+				respStr = HttpClientUtil.doGetRequest(url);
+			} else {
+				headMap = Common.stringToMap(header, ":", '|');
+				respStr = HttpClientUtil.doGetRequest(url, headMap);
+			}
+		}
+		//  response to map 
+		//  to-do 
+		
+		
+		return null;
+	}
+
+	@Test(parameters = "param")
+	public String f3(String param) {
+
+		if (!param.equals("aaaaa")) {
+			Reporter.log("f3() again");
+			f3(param + "a");
+		}
+
 		Reporter.log("testNG testcase f3()");
 		assertTrue(true);
+
+		return "ok";
 	}
 
 }
--- src/main/java/com/zsdk/autotest/common/ExcelUtils.java
+++ src/main/java/com/zsdk/autotest/common/ExcelUtils.java
@@ -8,11 +8,12 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import javafx.scene.control.Cell;
+import jxl.Range;
 import jxl.Sheet;
 import jxl.Workbook;
 
 public class ExcelUtils {
-	
+
 	public Object[][] ExcelUtils(String fPath, String sheetName) {
 		return getSheetData(fPath, sheetName);
 	}
@@ -30,18 +31,17 @@ public class ExcelUtils {
 	public static Map<String, HashMap> toMapBySheet(String fPath, String sheetName) {
 		try {
 			File fExcel = getExcelFileObj(fPath);
-			Workbook wb = Workbook.getWorkbook(fExcel);
-			Sheet sht = wb.getSheet(sheetName);
+			Workbook book = Workbook.getWorkbook(fExcel);
+			Sheet sht = book.getSheet(sheetName);
 			int rowCount = sht.getRows();
 			int colCount = sht.getColumns();
-			System.out.println("中文");
-			System.out.println("rowCount:" + rowCount + ",colCount:" + colCount);
+
 			Cell cel = null;
 			Map<String, HashMap> excelContents = new HashMap();
 			if (rowCount < 2) {
 				System.out.println("no case");
 				return null;
-			} 
+			}
 
 			// 检查是否存在空行
 			String rowContents = "";
@@ -59,20 +59,19 @@ public class ExcelUtils {
 					}
 				}
 			}
-			
+
 			// 开始读取内容
 			for (int rowIndex = 1; rowIndex < rowCount; rowIndex++) {
 				HashMap<String, String> rowMap = new HashMap();
 				String testCaseCode = sht.getCell(0, rowIndex).getContents().toString();
 				for (int colIndex = 1; colIndex < colCount; colIndex++) {
-					rowMap.put(sht.getCell(colIndex, 0).getContents().toString(), sht.getCell(colIndex, rowIndex).getContents().toString());
+					rowMap.put(sht.getCell(colIndex, 0).getContents().toString(),
+							sht.getCell(colIndex, rowIndex).getContents().toString());
 				}
 				excelContents.put(testCaseCode, rowMap);
 			}
-			wb.close();
-			// HashMap<String, String> tmpMap = new HashMap();
-			// tmpMap.put("count", "" + (rowCount-2));
-			// excelContents.put("testsCount", tmpMap);
+			book.close();
+
 			return excelContents;
 		} catch (Exception e) {
 			System.out.println("发生异常：" + e);
@@ -80,59 +79,189 @@ public class ExcelUtils {
 		return null;
 	}
 
-	
+	/**
+	 * 从excel中获取用例--无测试步骤
+	 * @param fPath  excel路径
+	 * @param sheetName sheet name
+	 * @return Map<String, Object>[][]
+	 */
 	public static Object[][] getSheetData(String fPath, String sheetName) {
 		File file = new File(fPath);
-        Workbook book = null;
-        Sheet sheet = null;
-        try {
-            book = Workbook.getWorkbook(file);
-            sheet = book.getSheet(sheetName);
-        } catch (Exception e) {
-            System.out.println(fPath + ".xls文件读取失败");
-        }
-        int rows = sheet.getRows();
-        int cols = sheet.getColumns();
-        // 如果行数少于2，则没有必要再继续运行下去了，不会返回数据，直接退出程序
-        if (rows <= 1) {
-            System.out.println(fPath + ".xls的" + sheetName + " sheet没有数据");
-            System.exit(0);
-        }
-        // 为了返回值是Object[][]类型，定义一个多行单列的二维数组
-        HashMap<String, String>[][] arrMap = new HashMap[rows-1][1];
-        for (int i = 0; i < rows - 1; i++) {
-            arrMap[i][0] = new HashMap<>();
-        }
-        // 存储第一行的标题，作为key用
-        List<String> list = new ArrayList<String>();
-        for (int c = 0; c < cols; c++) {
-            list.add(sheet.getCell(c, 0).getContents());
-        }
-        // 以键值对的形式，存进一个哈希表里
-        for (int r = 1; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                arrMap[r-1][0].put(list.get(c), sheet.getCell(c, r).getContents());
-            }
-        }
-        book.close();
-        return arrMap;
+		Workbook book = null;
+		Sheet sheet = null;
+		try {
+			book = Workbook.getWorkbook(file);
+			sheet = book.getSheet(sheetName);
+		} catch (Exception e) {
+			System.out.println(fPath + ".xls文件读取失败");
+		}
+		int rows = sheet.getRows();
+		int cols = sheet.getColumns();
+		// 如果行数少于2，则没有必要再继续运行下去了，不会返回数据，直接退出程序
+		if (rows <= 1) {
+			System.out.println(fPath + ".xls的" + sheetName + " sheet没有数据");
+			System.exit(0);
+		}
+		// 为了返回值是Object[][]类型，定义一个多行单列的二维数组
+		HashMap<String, String>[][] arrMap = new HashMap[rows-1][1];
+		for (int i = 0; i < rows - 1; i++) {
+			arrMap[i][0] = new HashMap<>();
+		}
+		// 存储第一行的标题，作为key用
+		List<String> list = new ArrayList<String>();
+		for (int c = 0; c < cols; c++) {
+			list.add(sheet.getCell(c, 0).getContents());
+		}
+		// 以键值对的形式，存进一个哈希表里
+		for (int r = 1; r < rows; r++) {
+			for (int c = 0; c < cols; c++) {
+				arrMap[r - 1][0].put(list.get(c), sheet.getCell(c, r).getContents());
+			}
+		}
+		book.close();
+		return arrMap;
+	}
+	
+	/**
+	 * 从excel中获取用例--有测试步骤
+	 * @param fPath  excel路径
+	 * @param sheetName sheet name
+	 * @return Map<String, Object>[][]
+	 */
+	public static Object[][] getSheetData2(String fPath, String sheetName) {
+		File file = new File(fPath);
+		Workbook book = null;
+		Sheet sheet = null;
+		try {
+			book = Workbook.getWorkbook(file);
+			sheet = book.getSheet(sheetName);
+		} catch (Exception e) {
+			System.out.println(fPath + ".xls文件读取失败");
+		}
+		int rows = sheet.getRows();
+		int cols = sheet.getColumns();
+		
+		// 如果行数少于2，则没有必要再继续运行下去了，不会返回数据，直接退出程序
+		if (rows <= 1) {
+			System.out.println(fPath + ".xls的" + sheetName + " sheet没有数据");
+			System.exit(0);
+		}
+		
+		// 存储第一行的标题，作为key用
+		List<String> headList = new ArrayList<String>();
+		for (int c = 0; c < cols; c++) {
+			headList.add(sheet.getCell(c, 0).getContents());
+		}
+		// 测试用例名称,便于后面用例排序
+		List<String> caseNameList = new ArrayList<String>();
+		for (int c = 1; c < rows; c++) {
+			String caseName = sheet.getCell(0, c).getContents();
+			if (caseName.isEmpty() || caseName==null || ("").equals(caseName)) {
+				continue;
+			}
+			caseNameList.add(caseName);
+		}
+		
+		Map<String, Map<String, String>[]> tmpCases = new HashMap<>();
+		Map<String, String>[] caseSteps = null;
+		// 获取合并单元格
+		Range[] rangeCell = sheet.getMergedCells();
+		// 合并单元格用例提取
+		for (Range m : rangeCell) {
+			int mTopRow = m.getTopLeft().getRow();
+			int mBottomRow = m.getBottomRight().getRow();
+			String caseName = sheet.getCell(m.getTopLeft().getColumn(), m.getTopLeft().getRow()).getContents();
+			caseSteps = new HashMap[mBottomRow-mTopRow+1];
+			for (int i = 0; i < caseSteps.length; i++) {
+				Map<String, String> rowMap = new HashMap<>();
+				for (int col = 0; col < cols; col++) {
+					rowMap.put(headList.get(col), sheet.getCell(col,mTopRow+i).getContents());
+				}
+				caseSteps[i] = rowMap;
+			}
+			tmpCases.put(caseName, caseSteps);
+		}
+		
+		// 非合并单元格用例提取
+		for (int row = 1; row < rows; row++) {
+			boolean isInRange = false;
+			String caseName = null;
+			// 判断是否在合并单元格
+			for (Range m : rangeCell) {
+				int mTopRow = m.getTopLeft().getRow();
+				int mBottomRow = m.getBottomRight().getRow();
+				if (row>=mTopRow && row<= mBottomRow) { 
+					isInRange = true;
+					break;
+				}
+			}
+			if (! isInRange) { // 不在合并单元格
+				Map<String, String> rowMap = new HashMap<>();
+				caseSteps = new HashMap[1];
+				for (int col = 0; col < cols; col++) {
+					rowMap.put(headList.get(col), sheet.getCell(col,row).getContents());
+					caseName = sheet.getCell(0,row).getContents();
+				}
+				caseSteps[0] = rowMap;
+				tmpCases.put(caseName, caseSteps);
+			}
+		}
+		book.close();
+		
+		//转为map[][]
+		Map<String, Object>[][] arrCaseMap = new HashMap[tmpCases.size()][1];
+		for (int i = 0; i < arrCaseMap.length; i++) {
+			arrCaseMap[i][0] = new HashMap<>();
+		}
+		int i=0;
+//		for (Entry<String, Map<String, String>[]> entry : tmpCases.entrySet()) {
+//			arrCaseMap[i][0].put("CaseName", entry.getKey());
+//			arrCaseMap[i][0].put("Steps", entry.getValue());
+//			++i;
+//		}
+		for (String key : tmpCases.keySet()) {
+			arrCaseMap[i][0].put("CaseName", key);
+			arrCaseMap[i][0].put("Steps", tmpCases.get(key));
+			++i;
+		}
+		
+		// 用例排序
+		Map<String, Object>[][] casesMap = new HashMap[tmpCases.size()][1];
+		for (int a=0; a<caseNameList.size();a++) {
+			casesMap[a][0] = new HashMap<>();
+			for (int j = 0; j < casesMap.length; j++) {
+				String caseName = (String) arrCaseMap[j][0].get("CaseName");
+				if (caseName.equals(caseNameList.get(a))) {
+					casesMap[a][0].put("CaseName", caseName);
+					casesMap[a][0].put("Steps", arrCaseMap[j][0].get("Steps"));
+				}
+			}
+		}
+		return casesMap;
 	}
 	
 	
 	
 	
 	
+	
 	public static void main(String[] args) {
-		HashMap<String, String>[][] arrMap = (HashMap<String, String>[][]) getSheetData("E:/workspace/auto_test/testcases.xls", "cases_a");
-		
-		for(int i=0; i< arrMap.length; ++i) {
-			HashMap<String, String> map = arrMap[i][0];
-			System.out.println(map);
-			
-			for(Entry<String, String> entry : map.entrySet()){
-				System.out.println(entry.getKey() + ":" + entry.getValue());
+		@SuppressWarnings("unchecked")
+		HashMap<String, Object>[][] arrMap = (HashMap<String, Object>[][]) getSheetData2(
+				"E:/workspace/auto_test/testcasesa.xls", "cases_a");
+
+		for (int i = 0; i < arrMap.length; ++i) {
+			HashMap<String, Object> map = arrMap[i][0];
+
+			System.out.println( "CaseName:" + map.get("CaseName"));
+			Map<String, String>[] steps = (Map<String, String>[]) map.get("Steps");
+			for (int j = 0; j < steps.length; j++) {
+				Map<String, String> map2 = steps[j];
+				System.out.println("step:" + map2);
+//				for (Entry<String, String> entry : map2.entrySet()) {
+//					System.out.println(map.get("CaseName") + "===step:" + map2.get("Step") + "==" + entry.getKey() + ":" + entry.getValue());
+//				}
 			}
-			System.out.println(map.get("casename"));
 		}
 	}
 }
